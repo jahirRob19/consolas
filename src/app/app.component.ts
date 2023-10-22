e90cf0d4c26aef35a715d28d175f329639cb5d88
jahirRob19 <jahirrobles2020@gmail.com>
2023-10-22 16:58:37 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pagina';
+  autor: string = 'Jahir Leon';
+  location:string='';
+  numero: string = '656-201-91-27';
 }
